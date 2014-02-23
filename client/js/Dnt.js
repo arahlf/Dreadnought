ad0ef70e05d223b55ac3d413c32d49e12cef4323
arahlf <arahlf@gmail.com>
2014-02-23 15:01:36 -0600

    Reinventin' dat wheel... (added my own class system)

--- client/js/Dnt.js
+++ client/js/Dnt.js
@@ -1,6 +1,81 @@
-window.Dnt = {
+(function() {
 
-    WIDTH: 600,
-    HEIGHT: 400
+    function DntBaseClass() {}
 
-};
+    DntBaseClass.prototype.constructor = function() {};
+
+    DntBaseClass.prototype.callParent = function(args) {
+        var method = arguments.callee.caller;
+        var parentClassMethod = method.$owner.$parentClass.prototype[method.$name];
+
+        if (!parentClassMethod) {
+            throw new Error('No parent class method found for ' + method.$owner.$className + '#' + method.$name);
+        }
+
+        parentClassMethod.apply(this, args);
+    };
+
+
+    window.Dnt = {
+
+        WIDTH: 600,
+        HEIGHT: 400,
+
+        abstractFn: function() {
+            throw new Error('Dnt.abstractFn called directly.');
+        },
+
+        define: function(className, parentClass, members) {
+
+            if (arguments.length == 2) {
+                members = parentClass;
+                parentClass = DntBaseClass;
+            }
+
+            var namespaces = className.split('.');
+            var namespace = window;
+            var simpleName = namespaces[namespaces.length - 1];
+
+            for (var i = 0 ; i < namespaces.length - 1; i++) {
+                var nextNamespaceName = namespaces[i];
+
+                if (!namespace[nextNamespaceName]) {
+                    namespace[nextNamespaceName] = {};
+                }
+
+                namespace = namespace[nextNamespaceName];
+            }
+
+            eval('function ' + simpleName + '() { this.constructor.apply(this, arguments); }');
+
+            var classFunction = eval(simpleName);
+
+            namespace[simpleName] = classFunction;
+
+            if (parentClass) {
+                classFunction.prototype = Object.create(parentClass.prototype);
+            }
+
+            classFunction.prototype.$parentClass = parentClass;
+            classFunction.prototype.$className = className;
+
+            window[className] = classFunction;
+
+            var name, member;
+            var prototype = classFunction.prototype;
+
+            for (name in members) {
+                if (members.hasOwnProperty(name)) {
+                    member = members[name];
+
+                    if (typeof member === 'function') {
+                        member.$name = name;
+                        member.$owner = prototype;
+                    }
+
+                    prototype[name] = member;
+                }
+            }
+        }
+    };
+})();
---
+++ test/DntDefineTests.js
@@ -0,0 +1,92 @@
+
+describe('Dnt.define', function(){
+
+    var output = [];
+
+    beforeEach(function() {
+        output.length = 0;
+    });
+
+    // some class definitions that are used throughout the tests
+    Dnt.define('Foo.bar.A', {
+        constructor: function(a) {
+            output.push('a#constructor');
+            this.a = a;
+        },
+        foo: function() {
+            output.push('a#foo');
+        },
+        bar: function() {
+            output.push('a#bar');
+        },
+        baz: function() {
+            output.push('a#baz');
+        },
+        yikes: function() {
+            this.callParent();
+        }
+    });
+
+    Dnt.define('Foo.bar.B', Foo.bar.A, {
+        constructor: function(a, b) {
+            this.callParent([a]);
+            output.push('b#constructor');
+            this.b = b;
+        },
+        foo: function() {
+            this.callParent();
+            output.push('b#foo');
+        },
+        bar: function() {
+            this.callParent();
+            output.push('b#bar');
+        }
+    });
+
+    Dnt.define('Foo.bar.C', Foo.bar.B, {
+        constructor: function(a, b, c) {
+            this.callParent([a, b]);
+            output.push('c#constructor');
+            this.c = c;
+        },
+        foo: function() {
+            this.callParent();
+            output.push('c#foo');
+        },
+        baz: function() {
+            this.callParent();
+            output.push('c#baz');
+        }
+    });
+
+    // the actual tests
+    it('should call entirely up the class method chain', function() {
+        var c = new Foo.bar.C();
+        c.foo();
+
+        output.should.eql(['a#constructor', 'b#constructor', 'c#constructor', 'a#foo', 'b#foo', 'c#foo']);
+    });
+
+    it('should call inherited methods that are not overridden', function() {
+        var c = new Foo.bar.C();
+        c.bar();
+        c.baz();
+
+        output.should.eql(['a#constructor', 'b#constructor', 'c#constructor', 'a#bar', 'b#bar', 'a#baz', 'c#baz']); 
+    });
+
+    it('should set class properties', function() {
+        var c = new Foo.bar.C(1, 2, '3');
+
+        c.a.should.equal(1);
+        c.b.should.equal(2);
+        c.c.should.equal('3');
+    })
+
+    it('should give a helpful error message, maybe?', function() {
+        var c = new Foo.bar.C();
+        var yikes = c.yikes.bind(c);
+        
+        yikes.should.throw(/No parent class method found for/);
+    });
+});
